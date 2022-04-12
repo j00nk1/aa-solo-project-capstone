42f61c136cbf5c2543c5756267320b0de5e02108
Junki Sato <j9sato11@gmail.com>
2022-04-12 15:18:13 -0600

    created Quote Model

---
+++ app/models/quotes.py
@@ -0,0 +1,14 @@
+from email.policy import default
+from .db import db
+
+# from datetime import datetime
+
+
+class Quote(db.Model):
+    __tablename__ = "quotes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    # created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
