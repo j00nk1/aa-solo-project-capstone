86eb43b124c98eb8c825a4b961e0526233d886ff
Junki Sato <j9sato11@gmail.com>
2022-04-18 20:40:35 -0600

    commented out unused vars pt2

--- react-app/src/components/Ranking.js/index.js
+++ react-app/src/components/Ranking.js/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSingleQuoteThunk } from "../../store/quotes";
@@ -9,7 +9,7 @@ function Ranking() {
   const dispatch = useDispatch();
   const { quote_id } = useParams();
   // const [users, setUsers] = useState([]);
-  const quoteRecordsObj = useSelector(state => state.records);
+  // const quoteRecordsObj = useSelector(state => state.records);
   // const sortedRecords = Object.values(quoteRecordsObj).sort(
   //   (a, b) => b.score - a.score
   // );
