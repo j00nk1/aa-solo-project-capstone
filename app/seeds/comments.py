11f9d6e1897d6d9301ec437eeb7ff4f3621937af
Junki Sato <j9sato11@gmail.com>
2022-04-20 17:56:22 -0600

    decreased record number to seed

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -9,10 +9,10 @@ def seed_comments():
     data = [
         Comment(
             user_id=randint(1, 10),
-            record_id=randint(1, 51),
+            record_id=randint(1, 150),
             content=lorem.sentence(),
         )
-        for x in range(50)
+        for x in range(75)
     ]
 
     db.session.add_all(data)
