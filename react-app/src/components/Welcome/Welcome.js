9955fd16563ac80d336113a668c0267462f8d692
Junki Sato <j9sato11@gmail.com>
2022-05-20 11:13:18 -0600

    changed welcome message

--- react-app/src/components/Welcome/Welcome.js
+++ react-app/src/components/Welcome/Welcome.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./Welcome.css";
 
 function Welcome() {
+  const { pathname } = useLocation();
+
+  const isLogin = pathname === "/login";
+
   return (
-    <div className="welcome_container">
-      <h1>Welcome To Quote Typing</h1>
+    <div className="welcome_container container_col">
+      <h1>
+        Welcome {isLogin ? "Back" : null} To{" "}
+        <span className="dur welcome_message">Quote Typing</span>!
+      </h1>
+      <p>Check your typing skills with famous quotes!!</p>
     </div>
   );
 }
